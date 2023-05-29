b5ca44e79092b7a96374a36f1a3ba273b16bcbb9
Md Maniruzzaman <mpmmonir152@gmail.com>
2023-05-29 06:51:31 +0600

    footer blade & style done with responsive

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
-{{-- <footer>        
+<footer>        
     <div class="footer-info">
         <div class="footer-width contact">
             <h2>GET IN TOUCH</h2>
             <ul>
                 <li>
                     <span><i class="fa-solid fa-phone"></i></span>
-                    <a href="#">+8801625-446671</a>
+                    <a href="#">+8801625446671</a>
                 </li>                    
                 <li>
                     <span><i class="fa-regular fa-envelope"></i></span>
@@ -14,7 +13,7 @@
                 </li>
                 <li>
                     <span><i class="fa-solid fa-location-dot"></i></span>
-                    <a>House No-13, Road No-3, Basoti Housing, Mirpur-2, Dhaka-1216</a>
+                    <a href="#">House No-13, Road No-3 <br> Basoti Housing, Mirpur-2 <br> Dhaka-1216</a>
                 </li>
                 <li>
                     <span></span>
@@ -26,116 +25,43 @@
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
-                <li><a href="#">Our Products</a></li>
-                <li><a href="#">FAQ</a></li>
                 <li><a href="#">Contact</a></li>
+                <li><a href="#">Our Products</a></li>                                       
             </ul>
         </div>
         <div class="footer-width about">
             <h2>ABOUT</h2>
             <p>
-                আমাদের নিজস্ব কারখানায় তৈরি
-                টি-শার্ট, পোলো শার্ট, জার্সি, টাউজার, ছোট-বড় সব ধরনের 
-                ওয়াডার নেওয়া হয় 
-                খুচরা এবং পাইকারি বাংলাদেশের সব জেলায় ডেলিভারি দেওয়া হয় 
-                কেউ নিতে ইচ্ছুক হলে আমাদের সাথে যোগাযোগ করতে পারেন 
+                Our own factory made t-shirts, polo shirts, jerseys, trousers,
+                 small and large waders are taken,
+                 retail and wholesale delivered to all districts of
+                  Bangladesh and world wide. 
             </p>
         </div>
     </div>
-=======
-<footer>
->>>>>>> acb28950ae57bfd88050b8e8a73e2cad657cfad3
     <div class="container">
-
         <div class="col-md-12 logo-box">
-            <div class="col-md-2">
+            <div class="col-md-3">
                 <a href="#"><img src="{{ asset('/assets/image/Footer') }}/logo.png" height="80px"; width="80px"></a>
             </div>
-            <div class="col-md-2"></div>
-            <div class="col-md-8 scl-icon">
-
-        <div class="row">
-            <div class="col-md-4"><h2>GET IN TOUCH</h2>
+            <div class="col-md-9 scl-icon">
                 <ul>
-                    <li>
-                        <span><i class="fa-solid fa-phone"></i></span>
-                        <a href="#">+8801625-446671</a>
-                    </li>                    
-                    <li>
-                        <span><i class="fa-regular fa-envelope"></i></span>
-                        <a href="#">bluejade58@gmail.com</a>
-                    </li>
-                    <li>
-                        <span><i class="fa-solid fa-location-dot"></i></span>
-                        <a>House No-13, Road No-3, Basoti Housing, Mirpur-2, Dhaka-1216</a>
-                    </li>
-                    <li>
-                        <span></span>
-                    </li>
-                </ul></div>
-            <div class="col-md-4">
-                <h2>QUICK LINKS</h2>
-
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Our Products</a></li>
-                    <li><a href="#">FAQ</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="https://www.facebook.com/bluejadebd/"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"></a><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"></a><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"></a><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="#"></a><i class="fab fa-youtube"></i></a></li>                        
                 </ul>
-            </div>
-            <div class="col-md-4"><h2>ABOUT</h2>
-                <p>
-                    আমাদের নিজস্ব কারখানায় তৈরি
-                    টি-শার্ট, পোলো শার্ট, জার্সি, টাউজার, ছোট-বড় সব ধরনের 
-                    ওয়াডার নেওয়া হয় 
-                    খুচরা এবং পাইকারি বাংলাদেশের সব জেলায় ডেলিভারি দেওয়া হয় 
-                    কেউ নিতে ইচ্ছুক হলে আমাদের সাথে যোগাযোগ করতে পারেন 
-                </p>
-            </div>
-        </div>
-    </div>   
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 logo-box">
-                <div class="row">
-                    <div class="col-md-4">
-                        <a href="#"><img src="{{ asset('/assets/image/Footer') }}/logo.png" height="80px"; width="80px"></a>
-                </div>
-                <div class="col-md-8 scl-icon">
-                    <ul>
-                        <li><i class="fab fa-facebook-f"></i></li>
-                        <li><i class="fab fa-twitter"></i></li>
-                        <li><i class="fab fa-instagram"></i></li>
-                        <li><i class="fab fa-linkedin-in"></i></li>
-                        <li><i class="fab fa-youtube"></i></li>                        
-                    </ul>
-                </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-12 copy-right">
-                <div class="row">
-                    <div class="col-md-8">
-                        <p>&copy; 2023 Copyright By <a href="#">Blue Jade BD</a></p>
-                        <p>Designed & Developed By <a href="#">X-Developer</a></p>                
-                    </div>
-                    <div class="col-md-4 condition">
-                        <a href="#">Teams & Condition | Contact</a>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
-    
-    
-
-    
-    
-
-        
-    
-    
+    <div class="col-md-12 copy-right">
+        <div class="col-md-8">
+            <p>&copy; 2023 Copyright By <a href="#">Blue Jade BD</a></p>
+            <p>Designed & Developed By <a href="#">X-Developer</a></p>                
+        </div>
+        <div class="col-md-4 condition">
+            <a href="#">Teams & Condition     |    Contact</a>
+        </div>
+    </div>
 </footer>
