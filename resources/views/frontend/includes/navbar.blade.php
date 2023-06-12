72bcbb158cdb166c6ee9b4ab8c4c63d9998fdad2
sharif1811 <sharifhossaion57@gmail.com>
2023-06-12 14:39:06 +0600

    new javascript add

--- resources/views/frontend/includes/navbar.blade.php
+++ resources/views/frontend/includes/navbar.blade.php
@@ -1,8 +1,6 @@
-
-
 <header>
-    <div class="container">
-        <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+    <div class="container-fluied header-section">
+        <nav class="navbar fix-position navbar-expand-lg sticky-top navbar-light bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{'/'}}">
                     <img width="50px" src="{{ asset('image/logo/logo.png') }}">
@@ -13,13 +11,13 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <div class="collapse navbar-collapse script-position" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="{{ '/' }}">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">ABOUT US</a>
+                            <a class="nav-link" href="">ABOUT US</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">NEWS</a>
@@ -42,5 +40,9 @@
                 </div>
             </div>
         </nav>
+
+        <div class="arrow">
+            <a href="#"><i class="fa-solid fa-arrow-up"></i></a>
+        </div>
 </header>
 
